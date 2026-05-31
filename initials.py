0f3e8ea7b616dca337d505ce606f818b67428f6f
stevewhyte626 <2425400806@live.gctu.edu.gh>
2026-05-31 14:37:00 +0000

    initials.py

--- initials.py
+++ initials.py
@@ -1,6 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Steven Whyte Dankyi
+# STUDENT ID: 2425400806
+# Email: 2425400806@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Steven Whyte Dankyi, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # Print initials S, W, D in block letters
+    print()
+    print("...SWD")
+    print()
+    print(".....SSSSSSSSSS........WW........WW........DDDDDDDDDD.......")
+    print("....SSSSSSSSSSS........WW........WW........DD........DD.....")
+    print("...SSSS................WW........WW........DD........DD.....")
+    print("...SSSS................WW........WW........DD........DD.....")
+    print(".....SSSSSS............WW........WW........DD........DD.....")
+    print(".......SSSSSS..........WW........WW........DD........DD.....")
+    print("...........SSSS........WW...WW...WW........DD........DD.....")
+    print("...........SSSS........WW...WW...WW........DD........DD.....")
+    print("...SSSSSSSSSSS....**...WW...WWW..WW...**...DD........DD...**")
+    print("...SSSSSSSSSS.....**...WWWWW..WWWWW...**...DDDDDDDDDD.....**")
+    print()
 
 
 
